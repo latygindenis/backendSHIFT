a066c3b5ca29e072a8b0a80afacfb8738df45f20
latygindenis <denisss2221@gmail.com>
2018-07-08 16:43:04 +0700

    Update AndroidPushNotificationsService.java

--- src/main/java/ftc/shift/sample/services/AndroidPushNotificationsService.java
+++ src/main/java/ftc/shift/sample/services/AndroidPushNotificationsService.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ExecutionException;
 
 @Service
 public class AndroidPushNotificationsService {
-	private static final String FIREBASE_SERVER_KEY = "key=AAAA3G-H6MY:APA91bEqmEpqJcnEMqXIFaFNtvqNp_XMMtGfEAj7F6eUjx9wtareEKplWT62bZe9PbwaQqZbrSEBv8ucVOoSu_BqvGbHbhZltAyITbqQofmD-PmYyUrn5GVtC4WTS0oW6Oqovoxx53B7IlvOb5xGqzKEvUtKLIVeDQ";
+	private static final String FIREBASE_SERVER_KEY = "key=";
 	private static final String FIREBASE_API_URL = "https://fcm.googleapis.com/fcm/send";
 
 
